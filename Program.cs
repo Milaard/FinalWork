8a93c21adce9215739751a8eebf3401d5c97f602
Milaard <116643003+Milaard@users.noreply.github.com>
2022-11-20 22:04:05 +0300

    Add Solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[]array = GetArray();
+//string[] array = new[] {"Hello", "2", "world", ":-"};
+string[] result = FilterArray(array, 3);
+
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+Console.ReadKey();
+
+string[] GetArray()
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+int CountLessThan(string[] input, int n)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if(input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+string[] FilterArray(string[] input, int n)
+{
+    int count = CountLessThan(input, n);
+    string[] output = new string[count];
+    int j = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+        
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
